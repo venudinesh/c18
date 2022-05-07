48bb09667b9d144572e37c5acc02a2f1f7be5f50
Vaisakh Murali <mvaisakh@statixos.com>
2022-05-07 12:23:14 +0100

    nashc: Import power and perf configs to device tree This would help changing perf and power HAL parameters in userspace.
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: Ieded2391e20a396ab5be52b2b465ffc950435b6e

--- device.mk
+++ device.mk
@@ -270,6 +270,11 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
+    $(LOCAL_PATH)/configs/perf/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
+    $(LOCAL_PATH)/configs/perf/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0.vendor \
@@ -306,6 +311,7 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     nfc_detect.sh
 
+# Fastboot
 PRODUCT_PACKAGES += \
     init.recovery.mt6785.rc
 
