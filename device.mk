f75c9fef34e26833718e611cf354573318064016
Alexander Koskovich <akoskovich@pm.me>
2022-12-13 19:45:02 +0000

    nashc: Load WiFi firmware from ODM
    Change-Id: Ie048cf85e0e4e5c325eb77fda1cbe32b21746aad

--- device.mk
+++ device.mk
@@ -114,7 +114,8 @@ PRODUCT_PACKAGES += \
     init.mt6785.usb.rc \
     init.project.rc \
     init.sensor_1_0.rc \
-    ueventd.mtk.rc
+    ueventd.mtk.rc \
+    ueventd.oplus.rc
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
