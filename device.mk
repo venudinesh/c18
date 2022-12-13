973ff98d24d395b9308f01b24f3c451d83509fa8
bengris32 <bengris32@protonmail.ch>
2022-12-13 21:01:29 +0000

    nashc: Build default Thermal 1.0 impl and 2.0 interface
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0299957bd1a56d6654bd9eb75c5f6643ac3debbe

--- device.mk
+++ device.mk
@@ -244,6 +244,11 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor \
+    android.hardware.thermal@1.0-impl
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.nashc
