742eb844b93188de794641cdf2277da68aaed5be
bengris32 <bengris32@protonmail.ch>
2022-12-18 21:20:10 +0000

    nashc: Initial SELinux rules * Also rework nfc_detect script to be inline with Android SEPolicy requirements.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0189f14885f5f8836fc14f085928f1171751163d

--- device.mk
+++ device.mk
@@ -326,6 +326,7 @@ PRODUCT_PACKAGES += \
     init.modem.rc \
     init.mt6785.rc \
     init.mt6785.usb.rc \
+    init.nfc_detect.rc \
     init.project.rc \
     init.sensor_1_0.rc \
     ueventd.mtk.rc \
