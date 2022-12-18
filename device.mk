569b3badf91535bc98d5976c15b0955ef3fe8a02
SamarV-121 <samarvispute121@pm.me>
2022-12-18 20:53:20 +0000

    nashc: Rename `libwifi-hal.so` to prevent conflict with AOSP
    Change-Id: Ia75aae1533b0f3d1cb759f21137f416fdeca9123

--- device.mk
+++ device.mk
@@ -401,7 +401,7 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy
+    android.hardware.wifi@1.0-service-lazy.nashc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
