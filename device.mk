ee2deb0b5b6908d8c134535d24973d5e2beea583
Iscle <albertiscle9@gmail.com>
2023-02-28 09:40:41 +0530

    spaced: Switch to reverse engineered V4A
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>
    
    v4a

--- device.mk
+++ device.mk
@@ -15,6 +15,11 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+=======
+$(call inherit-product-if-exists, packages/apps/prebuilt-apps/prebuilt-apps.mk)
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
@@ -408,8 +413,9 @@ PRODUCT_PACKAGES += \
 
 # Viper4Android
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/prebuilt/viper/libstdc++.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstdc++.so \
-    $(DEVICE_PATH)/prebuilt/viper/libv4a_fx.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libv4a_fx.so
+    $(LOCAL_PATH)/prebuilt/viper/lib/soundfx/libv4a_re.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libv4a_re.so \
+    $(LOCAL_PATH)/prebuilt/viper/lib64/soundfx/libv4a_re.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libv4a_re.so \
+    $(LOCAL_PATH)/prebuilt/viper/etc/audio_effects.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_effects.conf
 
 # VNDK
 PRODUCT_PACKAGES += \
