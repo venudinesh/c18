erfanoabdi/android_device_gigaset_GS5

nashc: Define Architecture flags
Signed-off-by: bengris32 <bengris32@protonmail.ch>
Change-Id: I6eed619e98c83d720b8eb72ca3fed25f68bffb33

BoardConfig.mk
@@ -6,5 +6,20 @@
 
 DEVICE_PATH := device/realme/nashc
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit the proprietary files
 include vendor/realme/nashc/BoardConfigVendor.mk

venudinesh/c18

BoardConfig.mk
@@ -6,5 +6,20 @@
 
 DEVICE_PATH := device/realme/nashc
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit the proprietary files
 include vendor/realme/nashc/BoardConfigVendor.mk