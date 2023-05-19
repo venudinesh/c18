18240e8eafddcd9c5fd0611dfd1e46adfcdc6770
Tim Zimmermann <tim@linux4.de>
2023-05-19 15:08:09 +0100

    nashc: Switch to new AIDL memtrack HAL
    Change-Id: I3ae077b5f59e4fcf32489eef64bce8551d758998

--- device.mk
+++ device.mk
@@ -115,8 +115,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@3.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor \
     android.hardware.graphics.composer@2.2-service \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack-service.mediatek-mali \
     libdrm.vendor \
     libdrm
 
