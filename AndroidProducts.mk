08c1fb2416a9bbe84f8e842f07a2376ba0a7d544
venudinesh <venudinesh21@gmail.com>
2023-08-25 19:19:08 +0530

    Colt BringUP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nashc.mk
+    $(LOCAL_DIR)/colt_nashc.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nashc-user \
-    lineage_nashc-userdebug \
-    lineage_nashc-eng
+    colt_nashc-user \
+    colt_nashc-userdebug \
+    colt_nashc-eng
