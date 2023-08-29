7d6f1c53147cd9b025ad5b1c52b8a93d60f2802e
venudinesh <venudinesh21@gmail.com>
2023-08-29 16:00:11 +0530

    Remove lineage health

--- device.mk
+++ device.mk
@@ -188,10 +188,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.nashc
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service-nashc
