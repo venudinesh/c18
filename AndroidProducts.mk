fe56a8f2f0b2952d6f219d343463b30d595c20be
venudinesh <venudinesh21@gmail.com>
2023-08-31 21:26:08 +0530

    Spark Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nashc.mk
+    $(LOCAL_DIR)/spark_nashc.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nashc-user \
-    lineage_nashc-userdebug \
-    lineage_nashc-eng
+    spark_nashc-user \
+    spark_nashc-userdebug \
+    spark_nashc-eng
--- spark_nashc.mk
+++ spark_nashc.mk
@@ -12,10 +12,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/realme/nashc/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_nashc
+#Spark stuff
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+NO_APERTURE := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+SPARK_BUILD_TYPE := Unofficial
+EXTRA_UDFPS_ANIMATIONS := true
+WITH_GAPPS=false
+
+PRODUCT_NAME := spark_nashc
 PRODUCT_DEVICE := nashc
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
