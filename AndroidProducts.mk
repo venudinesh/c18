523867a40749f2a470f57fac90bbbd0e17cfbaa2
eren <venudinesh21@gmail.com>
2023-09-25 00:09:36 +0530

    Ancient Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nashc.mk
+    $(LOCAL_DIR)/ancient_nashc.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nashc-user \
-    lineage_nashc-userdebug \
-    lineage_nashc-eng
+    ancient_nashc-user \
+    ancient_nashc-userdebug \
+    ancient_nashc-eng
---
+++ ancient_nashc.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/realme/nashc/device.mk)
+
+# Inherit some common AncientOS stuff.
+$(call inherit-product, vendor/ancient/config/common.mk)
+
+# Inherit some common AncientOS stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+# Official-ify
+ANCIENT_OFFICIAL=false
+
+PRODUCT_NAME := ancient_nashc
+PRODUCT_DEVICE := nashc
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme 8
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX3085L1 \
+    PRODUCT_NAME=RMX3085 \
+    PRIVATE_BUILD_DESC="RMX3085-user 12 SP1A.210812.016 R.GDPR.11887c9-1 release-keys"
+
+BUILD_FINGERPRINT := realme/RMX3085/RMX3085L1:12/SP1A.210812.016/R.GDPR.11887c9-1:user/release-keys
